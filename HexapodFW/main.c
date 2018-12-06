92c0928da9e0160b427abb050cc37843771c1774
Vladik Simontov <vladik@Vladiks-MacBook-Pro.local>
2018-12-05 22:36:06 -0800

    Added header file for main

--- HexapodFW/main.c
+++ HexapodFW/main.c
@@ -1,13 +1,38 @@
- #include <stdint.h>
- #include <stdbool.h>
- #include <stdio.h>
- #include "tm4c123gh6pm.h"
- #include "src/I2C.h"
- #include "src/Timer.h"
- #include "src/PCA9685.h"
- #include "src/Gaits.h"
- #include "src/UART.h"
- #include "src/Bluetooth.h"
+/*! \project Vorpal Hexapod on TIVA 
+*
+* \brief
+*   The project is based on the project designed Vorpal Robotics, LLC.
+*   Control functionality was ported from the Arduino to TIVA, while new driver
+*   functions (I2C, Bluetooth, Servo Driving, UART) were written by the authors 
+*   stated below.
+*
+*  \original codebase:
+*   Copyright (C) 2017, 2018 Vorpal Robotics, LLC.
+*   https://github.com/vorpalrobotics/VorpalHexapod
+*
+*
+* \author vsimontov
+*         livey
+*         clopez
+* \info
+* All licesning agreements defined by Vorpal Robotics, LLC are in effect
+*
+*
+* For details on programming, refer to TM4C123G datasheet :
+* http://www.ti.com/lit/ds/spms376e/spms376e.pdf
+*
+******************************************************************************/ 
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "tm4c123gh6pm.h"
+#include "src/I2C.h"
+#include "src/Timer.h"
+#include "src/PCA9685.h"
+#include "src/Gaits.h"
+#include "src/UART.h"
+#include "src/Bluetooth.h"
 
  // Other initializations
 
@@ -24,9 +49,10 @@ int main(void) {
    PCA9685_UpdatePWMFrequency(60u);
    PCA9685_Restart();
    
-   //Confirm I2C/PCA: chill until we send a wakeup command
+   //Stand the Hexapod, run a demo
    stand();
-    demo();
+   demo();
+   
    //Initialize UART module 1, pin PC4 is Rx
    BlueTooth_Init(); 
    
